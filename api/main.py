88c459a9ccce838cb7b02794021d3ec6d2468bc3
Markus Antonsen <markus-antonsen@hotmail.com>
2022-12-22 18:07:29 +0100

    Preprocess the image exacly like training before it predicts

--- api/main.py
+++ api/main.py
@@ -5,6 +5,8 @@ import wildfire_control
 from PIL import Image
 import numpy as np
 import cv2
+from keras_preprocessing.image import img_to_array, ImageDataGenerator
+from keras.applications.vgg16 import preprocess_input
 
 """
 Using uvicorn to run API.
@@ -46,8 +48,10 @@ def upload_button():
 def classify_image(image: bytes = File(...)):
     # Loads the image uploaded and resizes it
     image = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_UNCHANGED)
-    image = cv2.resize(image, (250, 250))
-    image = [image]
-    image = np.array(image)
+    image = cv2.resize(image, (224, 224))
+    image = img_to_array(image)
+    image =  image.reshape((1,  image.shape[0],    image.shape[1],  image.shape[2]))
+    image = preprocess_input(image)
     result = wildfire_control.run_model(image)
+    print(result)
     return f"{result}"
--- api/wildfire_control.py
+++ api/wildfire_control.py
@@ -2,18 +2,22 @@ import pickle
 import numpy as np
 import os
 import cv2
+import keras.models
 
 def load_image(image_path):
     image = cv2.imread(image_path)
     return [image]
 
 def import_model(model_file):
-    with open(model_file, "rb") as f:
-        model = pickle.load(f)
-    return model
+    return keras.models.load_model(model_file)
 
 def run_model(image):
-    model = import_model("C:\Code\Applied-ai\CNN50accuracy.sav")
-    result = model.predict(image)
-    return result
+    model = import_model("C:\Code\Applied-ai\model(2).h5")
+    pred = model.predict(image)
+    pred = np.argmax(pred,axis=1)
+    # labels = ({'fire': 0, 'no-fire': 1})
+    # labels = dict((v,k) for k,v in labels.items())
+    # pred = [labels[k] for k in pred]
+    pred = [np.round(x) for x in pred]
+    return pred
     
