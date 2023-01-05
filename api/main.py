555b5dc5efe5e10fbaba573e08db968420cab477
Markus Antonsen <markus-antonsen@hotmail.com>
2023-01-05 16:09:59 +0100

    Post function now clearly resturns that model predicted and replaced opencv with pillow.

--- api/main.py
+++ api/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, File
 from fastapi.responses import HTMLResponse
-import os
 import wildfire_control
-from PIL import Image
 import numpy as np
-import cv2
 from keras_preprocessing.image import img_to_array, ImageDataGenerator
-from keras.applications.vgg16 import preprocess_input
-
+from keras.applications.mobilenet_v2 import preprocess_input
+from keras.utils import load_img
+from PIL import Image
+import io
 
 """
 Using uvicorn to run API.
@@ -17,44 +16,28 @@ and then in the console type uvicorn main:app to see api.
 
 app = FastAPI()
 
-UPLOAD_FOLDER = "user_uploads"
-
-@app.get('/')
-def index():
-    return "Hello world"
-
-
 @app.get('/classify-image')
 def upload_button():
+
     html =  """
+    <body>
+    <h1>Image Classification</h1>
+    <p>Select an image to classify:</p>
     <form id="upload-form" method="post" enctype="multipart/form-data">
       <input type="file" name="image" accept="image/*">
       <button type="submit">Upload</button>
     </form>
     """
-    return HTMLResponse(html)
-   
-# @app.post('/wildfire')
-# def get_image(image: bytes = File(...)):
-#     # create the uploads folder if it doesn't exist
-#     if not os.path.exists(UPLOAD_FOLDER):
-#         os.makedirs(UPLOAD_FOLDER)
-#     # save the image to the uploads folder
-#     with open(os.path.join(UPLOAD_FOLDER, "image.jpg"), "wb") as f:
-#         f.write(image)
-#     return "File uploaded successfully"
 
+    return HTMLResponse(html)
 
 @app.post("/classify-image")
 def classify_image(image: bytes = File(...)):
     # Loads the image uploaded and resizes it
-    image = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_UNCHANGED)
-    image = cv2.resize(image, (224, 224))
-
-
+    image = Image.open(io.BytesIO(image))
+    image = image.resize((250, 250))
     image = img_to_array(image)
     image =  image.reshape((1,  image.shape[0],    image.shape[1],  image.shape[2]))
     image = preprocess_input(image)
     result = wildfire_control.run_model(image)
-    print(result)
     return f"{result}"
--- api/wildfire_control.py
+++ api/wildfire_control.py
@@ -1,6 +1,4 @@
-import pickle
 import numpy as np
-import os
 import cv2
 import keras.models
 
@@ -12,12 +10,10 @@ def import_model(model_file):
     return keras.models.load_model(model_file)
 
 def run_model(image):
-    model = import_model("C:\Code\Applied-ai\model(2).h5")
+    model = import_model("C:\Code\Applied-ai\model_new(4).h5")
     pred = model.predict(image)
-    pred = np.argmax(pred,axis=1)
-    # labels = ({'fire': 0, 'no-fire': 1})
-    # labels = dict((v,k) for k,v in labels.items())
-    # pred = [labels[k] for k in pred]
-    pred = [np.round(x) for x in pred]
+    pred = np.argmax(pred, axis=1)[0]
+    labels = {0: 'Fire detected', 1: 'No fire detected'}
+    pred = labels[pred]
     return pred
     
