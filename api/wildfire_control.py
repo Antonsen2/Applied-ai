409e1b768b826984469140af8baeb78f412a9e74
meDracula <me.dracula@protonmail.com>
2023-01-11 19:02:28 +0100

    Client id generator

--- api/wildfire_control.py
+++ api/wildfire_control.py
@@ -1,5 +1,15 @@
+from uuid import uuid4
+
+
+CLIENT_ID_POOL = []
+
+
 def generate_client_id() -> str:
-    # TODO: Algorithm for unique id
-    # client_id -> Time based cryto with random salt
-    #   if client_id already in pool -> new salt
-    return b"2a34e"
+    """Unique id generator uuid4 from RFC 4122"""
+    client_id = str(uuid4()).encode()
+    CLIENT_ID_POOL.append(client_id)
+    return client_id
+
+
+async def remove_client_id(client_id: bytes):
+    CLIENT_ID_POOL.remove(client_id)
