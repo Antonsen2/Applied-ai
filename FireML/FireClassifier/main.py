fb9dc7bcb1702f7d5f0fd2a254064c7c11e0825d
meDracula <me.dracula@protonmail.com>
2023-01-31 00:01:37 +0100

    FireML logging

--- FireML/FireClassifier/main.py
+++ FireML/FireClassifier/main.py
@@ -1,12 +1,41 @@
+import os
 import asyncio
+import logging
+from enum import Enum
 from networking import run_server
+
+class LoggingLevel(Enum):
+    NOTSET = logging.NOTSET
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+    CRITICAL = logging.CRITICAL
+
+    @classmethod
+    def get_level(cls, level: str):
+        return cls.__members__.get(level, cls.NOTSET).value
+
+
+LOGGER_NAME = "main"
+FORMAT = "| %(asctime)s | %(levelname)s | %(name)s | %(message)s |"
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+LOGGER = logging.getLogger(LOGGER_NAME)
+
+
+def setup_logging():
+    level = os.getenv('LOG_LEVEL', None)
+    if level is not None:
+        logging_level = LoggingLevel.get_level(level.upper())
+        logging.root.setLevel(logging_level)
 
 
 def app():
-    # GET hashicorp vault
+    LOGGER.info("Starting app")
     loop = asyncio.new_event_loop()
     loop.run_until_complete(run_server())
 
 
 if __name__ == '__main__':
+    setup_logging()
     app()
--- FireML/FireClassifier/networking.py
+++ FireML/FireClassifier/networking.py
@@ -1,5 +1,6 @@
 import asyncio
 import socket
+import logging
 from aescipher import AESCipher
 from settings import get_encryption_key
 from prediction import model_predict, preprocess_image
@@ -10,11 +11,17 @@ HOST_SERVER = socket.gethostname()
 PORT_SERVER = 5000
 CHUNK_SIZE = 1024
 
+LOGGER_NAME = "networking"
+LOGGER = logging.getLogger(LOGGER_NAME)
 
 async def run_server():
     server = await asyncio.start_server(handler, HOST_SERVER, PORT_SERVER)
+    LOGGER.info("Server running on %s:%d", HOST_SERVER, PORT_SERVER)
+
     async with server:
         await server.serve_forever()
+
+    LOGGER.info("Server %s:%d closed", HOST_SERVER, PORT_SERVER)
 
 
 async def handler(reader: asyncio.StreamReader,
@@ -22,6 +29,9 @@ async def handler(reader: asyncio.StreamReader,
     init_data = await reader.read(CHUNK_SIZE)
     checksum, client_id = AES.decrypt(init_data.strip()).split()
     checksum = int(checksum)
+    client_id = client_id.decode()
+
+    LOGGER.info("Receiving client %s image size %d", client_id, checksum)
 
     # recv image
     data = b''
@@ -32,17 +42,26 @@ async def handler(reader: asyncio.StreamReader,
     data = AES.decrypt(data)
 
     # TODO verify checksum
+    LOGGER.debug("client %s image received, expected: %d; got: %d", client_id,
+                 checksum, len(data))
 
     # prepare image for model
     image = preprocess_image(data)
+    LOGGER.debug("client %s image preprocess for model prediction", client_id)
 
     # use model
+    LOGGER.debug("client %s starting image prediction", client_id)
     fire_prediction = model_predict(image)
+    LOGGER.info("client %s image prediction %s", client_id, fire_prediction)
 
     # send back result
+    client_id = client_id.encode()
     msg = client_id + b" " + fire_prediction.encode("utf-8")
     checksum = f"{len(msg)}".encode("utf-8")
     msg = checksum + b" " + msg
+
+    LOGGER.debug("client %s sending response %s", client_id.decode(), msg.decode())
+
     encrypted_msg = AES.encrypt(msg)
     response = encrypted_msg + b" " * (CHUNK_SIZE - len(encrypted_msg))
 
@@ -50,4 +69,5 @@ async def handler(reader: asyncio.StreamReader,
     await writer.drain()
 
     writer.close()
+    LOGGER.debug("Closed client %s socket communication", client_id.decode())
     await writer.wait_closed()
