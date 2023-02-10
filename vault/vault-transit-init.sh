5e6946219f0a71efee367d16a4470c6ececc90aa
Albin <91608635+meDracula@users.noreply.github.com>
2023-02-10 15:09:35 +0100

    Resolve mix indent file
    Minor fix to missing indentation of file

--- vault/vault-transit-init.sh
+++ vault/vault-transit-init.sh
@@ -3,13 +3,13 @@
 VAULT_RETRIES=5
 
 if [ -z "${VAULT_ADDR}" ]; then
-  export VAULT_ADDR="http://127.0.0.1:8200"
+	export VAULT_ADDR="http://127.0.0.1:8200"
 fi
 
 until vault status > /dev/null 2>&1 || [ "$VAULT_RETRIES" -eq 0 ]; do
 	VAULT_RETRIES=$((VAULT_RETRIES-1))
-    echo "Waiting for vault to start...: ${VAULT_RETRIES}"
-    sleep 1
+	echo "Waiting for vault to start...: ${VAULT_RETRIES}"
+	sleep 1
 done
 
 # login with root token at $VAULT_ADDR
