266b46b0639e259a6fa6bb1fb7b5f0655bf2a611
Markus Antonsen <markus-antonsen@hotmail.com>
2023-02-10 14:37:23 +0100

    updated encryption in dockerfile and vault transit

--- vault/vault-transit-init.sh
+++ vault/vault-transit-init.sh
@@ -2,17 +2,18 @@
 
 VAULT_RETRIES=5
 
-if [ -z $VAULT_ADDR ]; then
+if [ -z "${VAULT_ADDR}" ]; then
   export VAULT_ADDR="http://127.0.0.1:8200"
 fi
 
 until vault status > /dev/null 2>&1 || [ "$VAULT_RETRIES" -eq 0 ]; do
-    echo "Waiting for vault to start...: $(($VAULT_RETRIES--))"
+	VAULT_RETRIES=$((VAULT_RETRIES-1))
+    echo "Waiting for vault to start...: ${VAULT_RETRIES}"
     sleep 1
 done
 
 # login with root token at $VAULT_ADDR
-vault login $VAULT_TOKEN
+vault login
 
 # enable vault transit engine
 vault secrets enable transit
